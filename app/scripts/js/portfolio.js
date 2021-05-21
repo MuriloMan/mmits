0249e0ad5a133825b3e88fec2207a7f917b48480
MuriloMan <manzolimurilo@gmail.com>
2021-05-20 22:29:56 -0300

    Finished portfolio session

--- app/scripts/js/portfolio.js
+++ app/scripts/js/portfolio.js
@@ -1,9 +1,16 @@
 $(function () {
     $('#portfolio .moreDetail').on('click', function () {
-        $('#portfolio .portfolioDetailItem').each(function (i,e){
+        $('#portfolio .portfolioDetailItem').each(function (i, e) {
             $(e).addClass('d-none');
         });
         var id = $(this).attr('data-portfolioId');
         $('#' + id).removeClass('d-none');
+
+        $("html").animate(
+            {
+                scrollTop: $('#' + id).offset().top
+            },
+            800 //speed
+        );
     });
 });
